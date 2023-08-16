2dc15a2a66faa147f51f74fb304052b058b3d6b5
Christian E <christianelena92@gmail.com>
2023-08-16 12:00:16 -0500

    Composition, props, ts type assertion, style job list

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="app">
-    hello, hyrule
+    <!-- using data binding we are calling the prop (jobs) and set it equal to jobs data that was returned earlier  -->
+    <JobList :jobs="jobs" />
   </div>
 </template>
+ 
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+// reactive gets imported automatically
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import JobList from './components/JobsList.vue'; 
+
+import Job from "./types/Job";  
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: { JobList },
+  setup() { 
+    const jobs = ref<Job[]>([
+      { title: 'Farm worker', location: 'ion Ranch', salary: 40000, id: '1' },
+      { title: 'Fullstack Developer', location: 'Alabama', salary: 120000, id: '2' },
+      { title: 'Swimming Teacher', location: 'punjabi valley', salary: 25000, id: '3' },
+    ]);
+    return { jobs }; 
+  }
 });
 </script>
 
